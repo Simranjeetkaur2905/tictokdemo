4d04762b61a413f9df24cb26356d017ded1f7a53
simranjeet kaur <chakkalsimmi@gmail.com>
2020-01-07 19:22:37 -0500

    images changing

--- tictokdemo/ViewController.swift
+++ tictokdemo/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var activeplayer = 1
+    var gamestate = [0,0,0,0,0,0,0,0,0]
+    let winningcombinations = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnpressed(_ sender: UIButton) {
+        
+        print(sender.tag)
+        let activeposition = sender.tag - 1
+        if gamestate[activeposition] == 0{
+            gamestate[activeposition] = activeplayer
+            if activeplayer == 1{
+                sender.setImage(UIImage(named: "nought.png"), for: [])
+                activeplayer = 2
+            }
+            else {
+                sender.setImage(UIImage(named: "cross.png"), for: [])
+                activeplayer = 1
+            }
+        }
+        
+        
+    }
+    
 }
 
